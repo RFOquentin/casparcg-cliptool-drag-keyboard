85a34a15adf4ddbaf4a26c4c89a2668f6467f186
Mitaine <quentin.rfo@gmail.com>
2024-09-23 09:48:00 -0100

    Adjust

--- src/client/main-page/components/control-actions/control-actions.tsx
+++ src/client/main-page/components/control-actions/control-actions.tsx
@@ -4,9 +4,11 @@ import { ReduxSettingsService } from "../../../../shared/services/redux-settings
 import { AppNavigationService } from "../../../../shared/services/app-navigation-service";
 import { State } from "../../../../shared/reducers/index-reducer";
 import './control-actions.scss'
+import { OutputState } from "../../../../shared/models/settings-models";
 import Group from "../../../shared/components/group/group";
 import { BrowserService } from "../../../shared/services/browser-service";
 import Toggle from "../../../shared/components/toggle/toggle";
+import { SocketPlayService } from "../../../shared/services/socket-play-service";
 import { SocketService } from "../../../shared/services/socket-service";
 import { SocketSettingsService } from "../../../shared/services/socket-settings-service";
 
@@ -56,13 +58,24 @@ export default function ControlActions(): JSX.Element {
           checked={outputState.manualStartState}
           onChange={(isChecked) => toggleManualStartState(activeTabIndex, !isChecked)}>
             MANUAL
-        </Toggle>      
+        </Toggle>
+        {outputState.manualStartState && <button
+          className="control-action-start-button"
+          onClick={() => playCuedFile(activeTabIndex, outputState) }>
+            START
+        </button>}        
       </Group> 
     </>
   )
 }
 
-// Les fonctions existantes pour changer d'état
+function playCuedFile(activeTabIndex: number, outputState: OutputState): void {
+  if (!outputState.cuedFileName) {
+    return
+  }
+  new SocketPlayService(SocketService.instance.getSocket()).playFile(activeTabIndex, outputState.cuedFileName)
+}
+
 function toggleLoopState(activeTabIndex: number, isChecked: boolean): void {
   new SocketSettingsService(SocketService.instance.getSocket()).setLoopState(activeTabIndex, isChecked)
 }
